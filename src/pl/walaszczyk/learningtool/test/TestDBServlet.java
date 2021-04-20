26c8b5b6a5714ada60e68b9462a48d8c7a965f41
owca <walaszczyk.dawid@gmail.com>
2021-04-20 11:05:04 +0200

    preparing project structure

--- src/pl/walaszczyk/learningtool/test/TestDBServlet.java
+++ src/pl/walaszczyk/learningtool/test/TestDBServlet.java
@@ -16,8 +16,8 @@ import java.sql.*;
 public class TestDBServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	/*
+	 * DB Test purposes
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
